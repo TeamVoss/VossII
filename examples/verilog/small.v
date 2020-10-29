977966f657a77510cd13d420aa8511ea24003860
Markus <mararon@chalmers.se>
2020-10-29 10:29:43 +0100

    Working isomatch and new example.

--- examples/verilog/small.v
+++ examples/verilog/small.v
@@ -1,4 +1,18 @@
 // File: small.v
+module mymux2(
+  din_1,  // Mux Second input
+  din_0,  // Mux first input
+  sel,    // Select input
+  mux_out // Mux output
+);
+input din_0, din_1, sel;
+output mux_out;
+reg mux_out;
+
+assign mux_out = (sel) ? din_1 : din_0;
+
+endmodule
+
 module mux4(
   din_0,  // Mux first input
   din_1,  // Mux Second input
@@ -13,8 +27,8 @@ output mux_out;
 reg mux_out;
 reg mid01, mid23;
 
-mux2 mux1 (.din_0(din_0), .din_1(din_1), .sel(sel[0]), .mux_out(mid01));
-mux2 mux2 (.din_0(din_2), .din_1(din_3), .sel(sel[0]), .mux_out(mid23));
-mux2 mux12 (.din_0(mid01), .din_1(mid23), .sel(sel[1]), .mux_out(mux_out));
+mymux2 mux1 (.din_0(din_0), .din_1(din_1), .sel(sel[0]), .mux_out(mid01));
+mymux2 mux2 (.din_0(din_2), .din_1(din_3), .sel(sel[0]), .mux_out(mid23));
+mymux2 mux12 (.din_0(mid01), .din_1(mid23), .sel(sel[1]), .mux_out(mux_out));
 
 endmodule
