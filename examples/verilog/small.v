098069ec03d8afa6b130db07817b610b6a1c7292
Markus <mararon@chalmers.se>
2020-11-04 12:11:43 +0100

    Fixed it so that we can control the 'depth' at which we want our matching algo. in a circuit.

--- examples/verilog/small.v
+++ examples/verilog/small.v
@@ -31,7 +31,7 @@ reg mux_out;
 reg mid01, mid23;
 
 mux2 mux1 (.din_0(din_0), .din_1(din_1), .sel(sel[0]), .mux_out(mid01));
-mux2 mux2 (.din_0(din_2), .din_1(din_3), .sel(sel[0]), .mux_out(mid23));
+mux2 mux2 (.din_0(din_3), .din_1(din_2), .sel(sel[0]), .mux_out(mid23));
 mux2 mux12 (.din_0(mid01), .din_1(mid23), .sel(sel[1]), .mux_out(mux_out));
 
 endmodule
