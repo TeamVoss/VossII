433c6ee7274c76542c1046c46cfe55ff4385ec30
Markus <mararon@chalmers.se>
2021-02-18 10:47:34 +0100

    Hm.

--- src/bin/fl/iso.c
+++ src/bin/fl/iso.c
@@ -1,16 +1,15 @@
-//-------------------------------------------------------------------
+//------------------------------------------------------------------------------
 // Copyright 2020 Markus Aronsson
 // SPDX-License-Identifier: Apache-2.0
-//-------------------------------------------------------------------
+//------------------------------------------------------------------------------
 
-/************************************************************************/
-/*									*/
-/*     Original author: Markus Aronsson, 2020                           */
-/*									*/
-/************************************************************************/
+/******************************************************************************/
+/*      	                                      							  */
+/*     Original author: Markus Aronsson, 2020                                 */
+/*                                  									      */
+/******************************************************************************/
 #include "strings.h"
 #include "graph.h"
-#include "fsm.h"
 #include "iso.h"
 
 /******************************************************************************/
@@ -24,206 +23,117 @@
 /******************************************************************************/
 /*                              PRIVATE VARIABLES                             */
 /******************************************************************************/
-static rec_mgr     iso_mat_mgr;
-static iso_mat_ptr P; // Piece.
-static iso_mat_ptr G; // Puzzle.
-static rec_mgr     adj_mat_mgr;
-static adj_mat_ptr M;
-//
-static typeExp_ptr  fsm_handle_tp;
+// Isomatch.
+static rec_mgr mat_mgr;
+static mat_ptr P; // Piece/Needle    adj. matrix.
+static mat_ptr G; // Puzzle/Haystack adj. matrix.
+static mat_ptr M; // Isomatch.            matrix.
 
 // Forward definitions local functions -----------------------------------------
-static void recurse();
-static void prune();
-//
-static adj_mat_ptr create_adj_mat();
-static unint degree(adj_mat_ptr adj_mat, unint node);
-//
-static iso_mat_ptr create_iso_mat(fsm_ptr fsm);
-static void allocate_adjacency_matrix(adj_mat_ptr adj_mat, unint R, unint C);
-static void free_adjacency_matrix(adj_mat_ptr adj_mat);
-//
-static void initialize_isomatch(fsm_ptr piece, fsm_ptr puzzle);
-//
-static void test(g_ptr redex);
+// Matrix mgm.
+static bool** matrix_allocate(unint R, unint C);
+static void matrix_free(bool **m);
+// Isomatch.
+static bool test_isomorphism_formatting(mat_ptr iso);
+static bool test_isomorphism_match(mat_ptr iso, mat_ptr p, mat_ptr g);
 
 /******************************************************************************/
 /*                                LOCAL FUNCTIONS                             */
 /******************************************************************************/
 
-// recurse(used_columns, cur_row, G, P, M)
-//   if cur_row = num_rows(M)
-//     if M is an isomorphism:
-//        output yes and end the algorithm
-//   M' = M
-//   prune(M')
-//   for all unused columns c
-//     set column c in M' to 1 and other columns to 0
-//     mark c as used
-//     recurse(used_column, cur_row+1, G, P, M')
-//     mark c as unused
-//   output no
-static void
-recurse()
+// Matrix mgm. -----------------------------------------------------------------
+
+static bool**
+matrix_allocate(unint R, unint C)
 {
-
-}
-
-// prune(...)
-// do
-//   for all (i,j) where M is 1
-//     for all neighbors x of vi in P
-//       if there is no neighbor y of vj s.t. M(x,y)=1
-//         M(i,j)=0
-// while M was changed
-static void
-prune()
-{
-
-}
-
-// Adjacency matrix ------------------------------------------------------------
-
-static adj_mat_ptr
-create_adj_mat()
-{
-    unint _nodes_P = P->adj_mat->rows;
-    unint _nodes_G = G->adj_mat->rows;
-    // ----------------------------------------
-    // Allocate.
-    adj_mat_ptr adj_mat;
-    adj_mat = (adj_mat_ptr) new_rec(&adj_mat_mgr);
-    // todo: not sure about this one...
-    allocate_adjacency_matrix(adj_mat, _nodes_P, _nodes_G);
-    // ----------------------------------------
-    // Initialize.
-    for (unint i = 0; i<_nodes_P; i++) {
-	for (unint j = 0; j<_nodes_G; j++) {
-	    if (degree(P->adj_mat, i) >= degree(G->adj_mat, j)) {
-		continue;
-            }
-            // More checks.
-            // Add link.
-        }
-    }
-    //
-    return adj_mat;
-}
-
-static unint
-degree(adj_mat_ptr adj_mat, unint node)
-{
-    unint *row   = adj_mat->mat[node];
-    unint degree = 0;
-    for (unint i = 0; i<adj_mat->cols; i++) {
-	if (row[i] == 1) {
-	    degree++;
-        }
-    }
-    return degree;
-}
-
-//static unint
-//kind(adj_mat_ptr adj_mat, unint node)
-//{
-//    return 0;
-//}
-
-// Piece and Puzzle matrices ---------------------------------------------------
-
-static iso_mat_ptr
-create_iso_mat(fsm_ptr fsm)
-{
-    unint        _node_count = COUNT_BUF(&(fsm->nodes));
-    adj_mat_rec *_adj_mat;
-    hash_record *_node_names;
-    // Allocate. -------------------------------------------
-    iso_mat_ptr iso_mat;
-    iso_mat = (iso_mat_ptr) new_rec(&iso_mat_mgr);
-    //
-    _adj_mat = (iso_mat->adj_mat);
-    allocate_adjacency_matrix(_adj_mat, _node_count, _node_count);
-    //
-    _node_names = &(iso_mat->node_names);
-    create_hash(_node_names, _node_count, int_hash, int_equ);
-    // Initialize. -----------------------------------------
-    unint ix = 0;
-    nnode_ptr np;
-    FOR_BUF(&(fsm->nodes), nnode_rec, np) {
-        unint n_idx = np->idx;
-	if (*get_real_name(np->vec, n_idx-(np->vec->map->from)+1) == '!') {
-	    continue;
-	}
-        // Associate nodes with their index in the adj. matrix.
-        unint  n_ix;
-        unint *n_ix_ptr = (unint*) find_hash(_node_names, INT2PTR(n_idx));
-        if (n_ix_ptr == NULL) {
-	    n_ix = ix++;
-            insert_hash(_node_names, INT2PTR(n_idx), INT2PTR(n_ix));
-        } else {
-	    n_ix = *n_ix_ptr;
-        }
-        // Build adj. matrix.
-	for(idx_list_ptr ilp = np->fanouts; ilp != NULL; ilp = ilp->next) {
-	    ncomp_ptr cp = (ncomp_ptr) M_LOCATE_BUF(&(fsm->composites), ilp->idx);
-	    FOREACH_NODE(t_idx, cp->outs) {
-                unint  t_ix;
-                unint *t_ix_ptr = find_hash(_node_names, INT2PTR(t_idx));
-                if (t_ix_ptr == NULL) {
-		    t_ix = ix++;
-                    insert_hash(_node_names, INT2PTR(t_idx), INT2PTR(t_ix));
-                } else {
-		    t_ix = *t_ix_ptr;
-                }
-                _adj_mat->mat[n_ix][t_ix] = 1;
-            }
-	}
-    }
-    //
-    return iso_mat;
-}
-
-static void
-allocate_adjacency_matrix(adj_mat_ptr adj_mat, unint R, unint C)
-{
-    adj_mat->rows   = R;
-    adj_mat->cols   = C;
-    adj_mat->mat    = Malloc(R*sizeof(int*));
-    adj_mat->mat[0] = Calloc(R*C*sizeof(int));
+    bool **m;
+    m = Malloc(R*sizeof(bool*));
+    m[0] = Calloc(R*C*sizeof(bool));
     for(unint i=1; i<R; i++) {
-	adj_mat->mat[i] = adj_mat->mat[0]+i*C;
+		m[i] = m[0]+i*C;
     }
+    return m;
 }
 
 static void
-free_adjacency_matrix(adj_mat_ptr adj_mat)
+matrix_free(bool **m)
 {
-    Free((void *)adj_mat->mat[0]);
-    Free((void *)adj_mat->mat);
+    Free((void *)m[0]);
+    Free((void *)m);
+}
+
+// ? ---------------------------------------------------------------------------
+
+static void
+preload()
+{
+
 }
 
 static void
-initialize_isomatch(fsm_ptr piece, fsm_ptr puzzle)
+preload_needle(g_ptr pexlif)
 {
-    P = create_iso_mat(piece);
-    G = create_iso_mat(puzzle);
-    create_adj_mat();
+
 }
 
 static void
-test(g_ptr redex)
+preload_haystack(g_ptr pexlif)
 {
-    g_ptr l = GET_APPLY_LEFT(redex);
-    g_ptr r = GET_APPLY_RIGHT(redex);
-    g_ptr g_fsm;
-    EXTRACT_1_ARG(redex, g_fsm);
-    fsm_ptr fsm = (fsm_ptr) GET_EXT_OBJ(g_fsm);
-    MAKE_REDEX_NIL(redex);
-    //
-    create_iso_mat(fsm);
-    //
-    DEC_REF_CNT(l);
-    DEC_REF_CNT(r);
+
+}
+
+// Solution check --------------------------------------------------------------
+
+static bool
+test_isomorphism_formatting(mat_ptr iso)
+{
+    bool row;
+    bool *used = Calloc(iso->cols*sizeof(bool));
+    // Only one 'TRUE' allowed per row and column.
+    for(unint i=0; i<iso->rows; i++) {
+		row = FALSE;
+		for(unint j=0; j<iso->cols; j++) {
+            if(iso->mat[i][j]) {
+				if(row || used[j]) {
+					Free((void *)used);
+					return FALSE;
+                }
+                row = TRUE;
+                used[j] = TRUE;
+            }
+        }
+    }
+    Free((void *)used);
+    return TRUE;
+}
+
+static bool
+test_isomorphism_match(mat_ptr iso, mat_ptr p, mat_ptr g)
+{
+	bool t1,t2;
+    // P == ISO x (ISO x G)^T
+	for(unint i=0; i<iso->rows; i++) {
+		for(unint j=0; j<iso->rows; j++) {
+			t1 = FALSE;
+            for(unint k=0; k<iso->cols; k++) {
+				t2 = FALSE;
+                for(unint l=0; l<iso->cols; l++) {
+					if(iso->mat[l][j] && g->mat[k][l]) {
+						t2 = TRUE;
+                        break;
+                    }
+                }
+                if(iso->mat[i][k] && t2) {
+					t1 = TRUE;
+                    break;
+                }
+            }
+            if(p->mat[i][j] != t1) {
+				return FALSE;
+            }
+        }
+    }
+    return TRUE;
 }
 
 /******************************************************************************/
@@ -233,20 +143,12 @@ test(g_ptr redex)
 void
 Iso_Init()
 {
-    new_mgr(&iso_mat_mgr, sizeof(iso_mat_rec));
-    new_mgr(&adj_mat_mgr, sizeof(adj_mat_rec));
-    //
-    fsm_handle_tp = Get_Type("fsm", NULL, TP_INSERT_FULL_TYPE);
+    new_mgr(&mat_mgr, sizeof(mat_rec));
 }
 
 void
 Iso_Install_Functions()
 {
-    Add_ExtAPI_Function("test", "1", FALSE,
-			GLmake_arrow(
-			    fsm_handle_tp,
-			    GLmake_void()),
-			test);    
 
 }
 
@@ -257,16 +159,13 @@ Iso_Install_Functions()
 void
 _DuMMy_iso()
 {
-    recurse();
-    prune();
-    //
-    create_adj_mat();
-    degree(NULL,0);
-    //
-    create_iso_mat(NULL);
-    allocate_adjacency_matrix(NULL,0,0);
-    free_adjacency_matrix(NULL);
-    initialize_isomatch(NULL,NULL);
-    //
-    M->rows=0;
+    P = NULL;
+    G = NULL;
+    M = NULL;
+    matrix_free(NULL);
+    matrix_allocate(0,0);
+    prune(NULL,NULL,NULL);
+    test_isomorphism_formatting(NULL);
+    test_isomorphism_match(NULL,NULL,NULL);
+    recurse(NULL,NULL,NULL);
 }
