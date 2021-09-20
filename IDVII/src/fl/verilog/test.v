77cd7fc31e937342d39fe3a1e098d1ec04a9d831
Markus <mararon@chalmers.se>
2021-09-20 12:21:50 +0200

    Touched up pexlif2verilog, added support for 'some' multi-dim vectors.

--- IDVII/src/fl/verilog/test.v
+++ IDVII/src/fl/verilog/test.v
@@ -1,32 +1,32 @@
 module mbuf1 (
-    input [7 : 0] a,
-    output [7 : 0] out
+    input wire [7 : 0] a,
+    output wire [7 : 0] out
 );
 reg [7 : 0] mid;
-m1_ m1(.a(a[7:0]), .mid(mid[7:0]));
-m2_ m2(.mid(mid[7:0]), .out(out[7:0]));
+m1_ m1_(.a(a[7:0]), .mid(mid[7:0]));
+m2_ m2_(.mid(mid[7:0]), .out(out[7:0]));
 endmodule
 module m1_ (
-    input [7 : 0] a,
-    output [7 : 0] mid
+    input wire [7 : 0] a,
+    output wire [7 : 0] mid
 );
-mdraw_inverter1_1_ m1(.a(a[7:0]), ._tmp(mid[7:0]));
+mdraw_inverter1_1_ m1_1_(.a(a[7:0]), ._tmp(mid[7:0]));
 endmodule
 module mdraw_inverter1_1_ (
-    input [7 : 0] a,
-    output [7 : 0] _tmp
+    input wire [7 : 0] a,
+    output wire [7 : 0] _tmp
 );
-assign _tmp = ~a[7:0];
+assign _tmp = ~(a[7:0]);
 endmodule
 module m2_ (
-    input [7 : 0] mid,
-    output [7 : 0] out
+    input wire [7 : 0] mid,
+    output wire [7 : 0] out
 );
-mdraw_inverter1_2_ m1(.mid(mid[7:0]), ._tmp(out[7:0]));
+mdraw_inverter1_2_ m1_2_(.mid(mid[7:0]), ._tmp(out[7:0]));
 endmodule
 module mdraw_inverter1_2_ (
-    input [7 : 0] mid,
-    output [7 : 0] _tmp
+    input wire [7 : 0] mid,
+    output wire [7 : 0] _tmp
 );
-assign _tmp = ~mid[7:0];
+assign _tmp = ~(mid[7:0]);
 endmodule
