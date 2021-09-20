aaeb74cbb0aa94195601f8ebc7636cf347f01b13
Markus <mararon@chalmers.se>
2021-09-20 14:18:23 +0200

    Various string functions, valid identifiers, and no range for 1-bit wide variables.

--- IDVII/src/fl/verilog/test.v
+++ IDVII/src/fl/verilog/test.v
@@ -1,30 +1,77 @@
-module mbuf1 (
+module buf1 (
     input wire [7 : 0] a,
+    input wire b,
     output wire [7 : 0] out
 );
 reg [7 : 0] mid;
-m1_ m1_(.a(a[7:0]), .mid(mid[7:0]));
-m2_ m2_(.mid(mid[7:0]), .out(out[7:0]));
+m_1 anon_1(.b(b), .mid(mid[7:0]));
+m_2 anon_2(.a(a[7:0]), .mid(mid[7:0]));
+m_3 anon_3(.a(a[7:0]), .mid(mid[7:0]));
+m_4 anon_4(.a(a[7:0]), .mid(mid[7:0]));
+m_5 anon_5(.mid(mid[7:0]), .out(out[7:0]));
 endmodule
-module m1_ (
+module m_1 (
+    input wire b,
+    output wire [7 : 0] mid
+);
+draw_hfl_ZX_8_i1_1_1 anon_1_1(.b(b), ._tmp(mid[7:0]));
+endmodule
+module draw_hfl_ZX_8_i1_1_1 (
+    input wire b,
+    output wire [7 : 0] _tmp
+);
+assign _tmp = {{7{0}}, b};
+endmodule
+module m_2 (
     input wire [7 : 0] a,
     output wire [7 : 0] mid
 );
-mdraw_inverter1_1_ m1_1_(.a(a[7:0]), ._tmp(mid[7:0]));
+reg [7 : 0] _TmP_1;
+draw_input_2_2_1 anon_2_1(._tmp(_TmP_1[7:0]));
+draw_binary_arithm__2_2 anon_2_2(.a(a[7:0]), ._TmP_1(_TmP_1[7:0]), ._tmp(mid[7:0]));
 endmodule
-module mdraw_inverter1_1_ (
+module draw_input_2_2_1 ( output wire [7 : 0] _tmp
+);
+assign _tmp = 2;
+endmodule
+module draw_binary_arithm__2_2 (
+    input wire [7 : 0] a,
+    input wire [7 : 0] _TmP_1,
+    output wire [7 : 0] _tmp
+);
+assign _tmp = (a[7:0]) << (_TmP_1[7:0]);
+endmodule
+module m_3 (
+    input wire [7 : 0] a,
+    output wire [7 : 0] mid
+);
+draw_unary_arithm_0_3_1 anon_3_1(.a(a[7:0]), ._tmp(mid[7:0]));
+endmodule
+module draw_unary_arithm_0_3_1 (
+    input wire [7 : 0] a,
+    output wire [7 : 0] _tmp
+);
+assign _tmp = (a[7:0]) + (0);
+endmodule
+module m_4 (
+    input wire [7 : 0] a,
+    output wire [7 : 0] mid
+);
+draw_inverter_4_1 anon_4_1(.a(a[7:0]), ._tmp(mid[7:0]));
+endmodule
+module draw_inverter_4_1 (
     input wire [7 : 0] a,
     output wire [7 : 0] _tmp
 );
 assign _tmp = ~(a[7:0]);
 endmodule
-module m2_ (
+module m_5 (
     input wire [7 : 0] mid,
     output wire [7 : 0] out
 );
-mdraw_inverter1_2_ m1_2_(.mid(mid[7:0]), ._tmp(out[7:0]));
+draw_inverter_5_1 anon_5_1(.mid(mid[7:0]), ._tmp(out[7:0]));
 endmodule
-module mdraw_inverter1_2_ (
+module draw_inverter_5_1 (
     input wire [7 : 0] mid,
     output wire [7 : 0] _tmp
 );
