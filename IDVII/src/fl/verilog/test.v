87b5afab6d9d740c9a987fc5926bfba81ec290c1
Markus <mararon@chalmers.se>
2021-09-20 17:07:09 +0200

    Call SX/Slice var. error earlier.

--- IDVII/src/fl/verilog/test.v
+++ IDVII/src/fl/verilog/test.v
@@ -4,76 +4,34 @@ module buf1 (
     output wire [7 : 0] out
 );
 reg [7 : 0] mid;
-m_1 anon_1(.b(b), .mid(mid[7:0]));
-m_2 anon_2(.a(a[7:0]), .mid(mid[7:0]));
-m_3 anon_3(.a(a[7:0]), .mid(mid[7:0]));
-m_4 anon_4(.a(a[7:0]), .mid(mid[7:0]));
-m_5 anon_5(.mid(mid[7:0]), .out(out[7:0]));
+m_1 anon_1(.a(a[7:0]), .mid(mid[7:0]));
+m_2 anon_2(.b(b), .a(a[7:0]), .mid(mid[7:0]), .out(out[7:0]));
 endmodule
 module m_1 (
-    input wire b,
+    input wire [7 : 0] a,
     output wire [7 : 0] mid
 );
-draw_hfl_ZX_8_i1_1_1 anon_1_1(.b(b), ._tmp(mid[7:0]));
+draw_hfl_ZX_8_i12_1_1 anon_1_1(.a(a[7:0]), ._tmp(mid[7:0]));
 endmodule
-module draw_hfl_ZX_8_i1_1_1 (
-    input wire b,
+module draw_hfl_ZX_8_i12_1_1 (
+    input wire [7 : 0] a,
     output wire [7 : 0] _tmp
 );
-assign _tmp = {{7{0}}, b};
+assign _tmp = {{7{0}}, a[2]};
 endmodule
 module m_2 (
+    input wire b,
     input wire [7 : 0] a,
-    output wire [7 : 0] mid
-);
-reg [7 : 0] _TmP_1;
-draw_input_2_2_1 anon_2_1(._tmp(_TmP_1[7:0]));
-draw_binary_arithm__2_2 anon_2_2(.a(a[7:0]), ._TmP_1(_TmP_1[7:0]), ._tmp(mid[7:0]));
-endmodule
-module draw_input_2_2_1 ( output wire [7 : 0] _tmp
-);
-assign _tmp = 2;
-endmodule
-module draw_binary_arithm__2_2 (
-    input wire [7 : 0] a,
-    input wire [7 : 0] _TmP_1,
-    output wire [7 : 0] _tmp
-);
-assign _tmp = (a[7:0]) << (_TmP_1[7:0]);
-endmodule
-module m_3 (
-    input wire [7 : 0] a,
-    output wire [7 : 0] mid
-);
-draw_unary_arithm_0_3_1 anon_3_1(.a(a[7:0]), ._tmp(mid[7:0]));
-endmodule
-module draw_unary_arithm_0_3_1 (
-    input wire [7 : 0] a,
-    output wire [7 : 0] _tmp
-);
-assign _tmp = (a[7:0]) + (0);
-endmodule
-module m_4 (
-    input wire [7 : 0] a,
-    output wire [7 : 0] mid
-);
-draw_inverter_4_1 anon_4_1(.a(a[7:0]), ._tmp(mid[7:0]));
-endmodule
-module draw_inverter_4_1 (
-    input wire [7 : 0] a,
-    output wire [7 : 0] _tmp
-);
-assign _tmp = ~(a[7:0]);
-endmodule
-module m_5 (
     input wire [7 : 0] mid,
     output wire [7 : 0] out
 );
-draw_inverter_5_1 anon_5_1(.mid(mid[7:0]), ._tmp(out[7:0]));
+draw_ite_2_1 anon_2_1(.b(b), .a(a[7:0]), .mid(mid[7:0]), ._tmp(out[7:0]));
 endmodule
-module draw_inverter_5_1 (
+module draw_ite_2_1 (
+    input wire b,
+    input wire [7 : 0] a,
     input wire [7 : 0] mid,
     output wire [7 : 0] _tmp
 );
-assign _tmp = ~(mid[7:0]);
+assign _tmp = b ? a[7:0] : mid[7:0];
 endmodule
